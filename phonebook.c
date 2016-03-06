e5507c632f3e18ecb6b46142d1d584e9475a35b0
siyangyang <all381en@gmail.com>
2016-03-06 23:37:00 +0800

    Implemented AVL tree

--- phonebook.c
+++ phonebook.c
@@ -16,12 +16,13 @@ static int prefix##Free(entry *pHead);
 
 GEN_DEF(ll);
 GEN_DEF(avl);
-
 static impl implList[] = {
     GEN_INIT_STRUCT("linkedlist", ll),
     GEN_INIT_STRUCT("avltree", avl)
 };
-
+static void rightRotation(entry **parent,int *unbalanced);
+static void leftRotation(entry **parent,int *unbalanced);
+static int avlinsert(entry **parent,char lastname[],int * unbalanced);
 int initImpl(impl *opt, const char *implName)
 {
     int i;
@@ -79,22 +80,152 @@ static int llFree(entry *pHead)
 /* AVL Tree */
 
 typedef struct __AVLTREE_PRIV_DATA {
-    struct __AVLTREE_PRIV_DATA *pLeft;
-    struct __AVLTREE_PRIV_DATA *pRight;
-    int bf;
+    struct __PHONE_BOOK_ENTRY *pLeft;
+    struct __PHONE_BOOK_ENTRY *pRight;
+    int bf;//hl-hr
 } avlpriv;
 
-static entry *avlFindName(char lastname[], entry *pHead)
+static entry *avlFindName(char lastName[], entry *pHead)
 {
+    while (pHead != NULL) {
+        int result=strcasecmp(lastName, pHead->lastName);
+        if (result == 0)
+            return pHead;
+        else if(result > 0)
+            pHead = GET_PRIV_PTR(avlpriv,pHead)->pRight;
+        else
+            pHead = GET_PRIV_PTR(avlpriv,pHead)->pLeft;
+    }
     return NULL;
 }
 
 static int avlAppend(char lastName[], entry **ppHead, entry **pE)
 {
+    int a = 0;
+    avlinsert(ppHead,lastName,&a);
     return 0;
 }
 
 static int avlFree(entry *pHead)
 {
+    if(pHead!=NULL) {
+        avlFree(GET_PRIV_PTR(avlpriv, pHead)->pLeft);
+        avlFree(GET_PRIV_PTR(avlpriv, pHead)->pRight);
+        free(pHead);
+    }
     return 0;
+}
+static int avlinsert(entry **parent,char lastname[],int * unbalanced)
+{
+    if(*parent==NULL) {
+        *unbalanced=1;
+        *parent = ALLOC_ENTRY(avlpriv);
+        GET_PRIV_PTR(avlpriv, *parent)->pLeft = NULL;
+        GET_PRIV_PTR(avlpriv, *parent)->pRight = NULL;
+        strcpy((*parent)->lastName, lastname);
+        GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+    } else if(strcasecmp((*parent)->lastName,lastname)>0) {
+
+        avlinsert(&(GET_PRIV_PTR(avlpriv, *parent)->pLeft),lastname,unbalanced);
+        if(*unbalanced)
+            switch(GET_PRIV_PTR(avlpriv, *parent)->bf) {
+                case -1:
+                    GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+                    *unbalanced=0;
+                    break;
+                case 0:
+                    GET_PRIV_PTR(avlpriv, *parent)->bf=1;
+                    break;
+                case 1:
+                    leftRotation(parent,unbalanced);
+            }              //need to do Left r
+    } else if(strcasecmp((*parent)->lastName,lastname)<=0) {
+        avlinsert(&(GET_PRIV_PTR(avlpriv,*parent)->pRight),lastname,unbalanced);
+        if(*unbalanced)
+            switch(GET_PRIV_PTR(avlpriv, *parent)->bf) {
+                case 1:
+                    GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+                    *unbalanced=0;
+                    break;
+                case 0:
+                    GET_PRIV_PTR(avlpriv, *parent)->bf=-1;
+                    break;
+                case -1:
+                    rightRotation(parent,unbalanced);//   do right
+            }
+
+    }
+    return 1;
+
+}
+static void leftRotation(entry **parent,int *unbalanced)
+{
+    entry *grandchild;
+    entry *child;
+    child =GET_PRIV_PTR(avlpriv, *parent)->pLeft;
+    if(GET_PRIV_PTR(avlpriv, child)->bf==1) { // this is LL
+        GET_PRIV_PTR(avlpriv, *parent)->pLeft=GET_PRIV_PTR(avlpriv, child)->pRight;
+        GET_PRIV_PTR(avlpriv, child)->pRight=*parent;
+        GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+        *parent=child;
+    } else {
+        //do lr rotation
+        grandchild=GET_PRIV_PTR(avlpriv, child)->pRight;
+        GET_PRIV_PTR(avlpriv, child)->pRight=GET_PRIV_PTR(avlpriv, grandchild)->pLeft;
+        GET_PRIV_PTR(avlpriv, grandchild)->pLeft=child;
+        GET_PRIV_PTR(avlpriv, *parent)->pLeft=GET_PRIV_PTR(avlpriv, grandchild)->pRight;
+        GET_PRIV_PTR(avlpriv, grandchild)->pRight=*parent;
+        switch(GET_PRIV_PTR(avlpriv, grandchild)->bf) {
+            case 1:
+                GET_PRIV_PTR(avlpriv, *parent)->bf=-1;
+                GET_PRIV_PTR(avlpriv, child)->bf=0;
+                break;
+            case 0:
+                GET_PRIV_PTR(avlpriv, *parent)->bf=GET_PRIV_PTR(avlpriv, child)->bf=0;
+                break;
+            case -1:
+                GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+                GET_PRIV_PTR(avlpriv, child)->bf=1;
+        }
+        *parent=grandchild;
+
+    }
+    GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+    *unbalanced=0;
+}
+static void rightRotation(entry **parent,int *unbalanced)
+{
+    entry *grandchild;
+    entry *child;
+    child= GET_PRIV_PTR(avlpriv, *parent)->pRight;
+    if(GET_PRIV_PTR(avlpriv, child)->bf==-1) {
+        //rr
+        grandchild=GET_PRIV_PTR(avlpriv, child)->pRight;
+        GET_PRIV_PTR(avlpriv, *parent)->pRight=GET_PRIV_PTR(avlpriv, child)->pLeft;
+        GET_PRIV_PTR(avlpriv, child)->pLeft=*parent;
+        GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+        *parent=child;
+    } else {
+        //rl
+        grandchild=GET_PRIV_PTR(avlpriv, child)->pLeft;
+        GET_PRIV_PTR(avlpriv, child)->pLeft=GET_PRIV_PTR(avlpriv, grandchild)->pRight;
+        GET_PRIV_PTR(avlpriv, grandchild)->pRight=child;
+        GET_PRIV_PTR(avlpriv, *parent)->pRight=GET_PRIV_PTR(avlpriv, grandchild)->pLeft;
+        GET_PRIV_PTR(avlpriv, grandchild)->pLeft = *parent;
+        switch(GET_PRIV_PTR(avlpriv, grandchild)->bf) {
+            case 1:
+                GET_PRIV_PTR(avlpriv, *parent)->bf=-1;
+                GET_PRIV_PTR(avlpriv, child)->bf=0;
+                break;
+            case 0:
+                GET_PRIV_PTR(avlpriv, *parent)->bf=GET_PRIV_PTR(avlpriv, child)->bf=0;
+                break;
+            case -1:
+                GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+                GET_PRIV_PTR(avlpriv, child)->bf=1;
+        }
+        *parent=grandchild;
+    }
+    GET_PRIV_PTR(avlpriv, *parent)->bf=0;
+    *unbalanced=0;
 }
